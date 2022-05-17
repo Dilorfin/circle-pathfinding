723cd345ee9cc2ec8e8cec0824dac85dcfc89134
Dilorfin <pavlo_kuibida@epam.com>
2022-05-17 22:01:32 +0300

    Fix cycle dependency

---
+++ a-star/pathfinding.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "graph.hpp"
+
+// for codewars kata Graph should be init in shortest_path_length function
+double shortest_path_length(const Point& startPoint, const Point& endPoint, const std::vector<Circle>& circles, Graph& graph)
+{
+	// return length of shortest route from a to b, avoiding the interiors of the circles in c
+	return 0;
+}
--- codewars_structs.hpp
+++ codewars_structs.hpp
@@ -57,10 +57,3 @@ inline bool equal(const Point& a, const Point& b)
 	constexpr double max_error = 1e-8;
 	return distance(a, b) <= max_error;
 }
-
-// for codewars kata Graph should be init in shortest_path_length function
-double shortest_path_length(const Point& startPoint, const Point& endPoint, const std::vector<Circle>& circles, Graph& graph)
-{
-	// return length of shortest route from a to b, avoiding the interiors of the circles in c
-	return 0;
-}
